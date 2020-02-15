ea00ad8cb7ff3961ff62a150b2d056c0cea04d66
James <james.ibberson@elder-studios.co.uk>
2020-02-15 21:47:53 +0000

    Add database seeder with comments and users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,19 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Comment;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $users = factory(User::class, 10)->create();
+
+        $users->each(function (User $user) {
+            for ($count = 0; $count < 5; $count++) {
+                factory(Comment::class)->create(['user_id' => $user->id]);
+            }
+        });
     }
 }
