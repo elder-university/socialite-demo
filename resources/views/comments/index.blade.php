864f43370a1a4708adaf50bc894b2a367bbb0cfb
James <james.ibberson@elder-studios.co.uk>
2020-02-15 23:19:42 +0000

    Add comments.destroy partial to comments.index

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -6,21 +6,11 @@
         <div class="card mb-4">
             <div class="card-header d-flex justify-content-between align-items-center">
                 {{ $comment->user->name }}
-{{--                <form action="{{ route('comments.destroy', $comment) }}" method="POST">--}}
-{{--                    @csrf--}}
-{{--                    @method('DELETE')--}}
-{{--                    <input type="submit" value="Delete Comment" class="btn btn-dark">--}}
-{{--                </form>--}}
+                @include('comments.partials.destroy')
             </div>
             <div class="card-body">
-                <h5 class="card-title">
-                </h5>
-                <h6 class="card-subtitle mb-2 text-muted">
-                    {{ $comment->created_at->diffForHumans() }}
-                </h6>
-                <p class="card-text">
-                    {{ $comment->body }}
-                </p>
+                <h6 class="card-subtitle mb-2 text-muted">{{ $comment->created_at->diffForHumans() }}</h6>
+                <p class="card-text">{{ $comment->body }}</p>
             </div>
         </div>
     @endforeach
