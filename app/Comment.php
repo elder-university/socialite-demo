bdaad6961b966aff721b96c151d894e7fc9eeedc
James <james.ibberson@elder-studios.co.uk>
2020-02-15 22:40:06 +0000

    Add user relationship to comment model

--- app/Comment.php
+++ app/Comment.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @mixin Builder
+ */
 class Comment extends Model
 {
-
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
