4e8e694d0d64cc7afa50cb230be139daef0ce964
James <james.ibberson@elder-studios.co.uk>
2020-02-15 21:34:12 +0000

    Add index action to comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+    const PER_PAGE = 5;
 
+    protected $comment;
+
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function index()
+    {
+        return view('comments.index')->with([
+            'comments' => $this->paginateComments()
+        ]);
+    }
+
+    private function paginateComments()
+    {
+        return $this->comment->orderByDesc('created_at')->paginate(self::PER_PAGE);
+    }
 }
