6308978a24d8a76d23df062bd123aa2f735742dd
James <james.ibberson@elder-studios.co.uk>
2020-02-16 22:01:35 +0000

    Tweak GitHubAuthController
    Previously didn't authenticate the resolved user from GitHub user

--- app/Http/Controllers/GitHubAuthController.php
+++ app/Http/Controllers/GitHubAuthController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 use Laravel\Socialite\Contracts\User as SocialiteUser;
 use Laravel\Socialite\Facades\Socialite;
 
 class GitHubAuthController extends Controller
 {
+    const REMEMBER_ME = true;
+
     public function auth()
     {
         return Socialite::driver('github')->redirect();
@@ -16,16 +19,18 @@ class GitHubAuthController extends Controller
 
     public function callback()
     {
-        $this->findOrCreateUser(
+        $user = $this->findOrCreateUser(
             Socialite::driver('github')->user()
         );
+
+        Auth::login($user, self::REMEMBER_ME);
 
         return redirect(route('comments.index'));
     }
 
     private function findOrCreateUser(SocialiteUser $gitHubUser)
     {
-        User::firstOrCreate(['github_id' => $gitHubUser->getId()], [
+        return User::firstOrCreate(['github_id' => $gitHubUser->getId()], [
             'name' => $gitHubUser->getName(),
             'email' => $gitHubUser->getEmail(),
             'password' => bcrypt(Str::random(32)),
