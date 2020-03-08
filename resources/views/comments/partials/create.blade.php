f4801d132af704526962343e5a1d2e3e7c4f22bf
James <james.ibberson@elder-studios.co.uk>
2020-03-08 14:37:00 +0000

    Add card around create comment form

--- resources/views/comments/partials/create.blade.php
+++ resources/views/comments/partials/create.blade.php
@@ -1,10 +1,14 @@
-<form action="{{ route('comments.store') }}" method="POST" class="mb-4">
-    @csrf
-    <div class="form-group">
-        <label for="body">Comment</label>
-        <textarea name="body" rows="5" class="form-control"></textarea>
+<div class="card mb-4">
+    <div class="card-header">Create a Comment</div>
+    <div class="card-body">
+        <form action="{{ route('comments.store') }}" method="POST" class="mb-4">
+            @csrf
+            <div class="form-group">
+                <textarea rows="5" class="form-control"></textarea>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Save">
+            </div>
+        </form>
     </div>
-    <div class="form-group">
-        <input type="submit" class="btn btn-primary" value="Save">
-    </div>
-</form>
+</div>
